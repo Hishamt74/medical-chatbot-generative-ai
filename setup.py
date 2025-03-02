ff4092cf9aae84db138185715ea29ed1280aadbe
Mohammed Hisham T <mohammedhishamt2002@gmail.com>
2025-03-02 14:06:41 +0530

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import find_packages,setup
+setup(
+    name="Generative AI Project",
+    packages=find_packages(),
+    version="0.1.0",
+    author_email="mohammedhishamt2002@gmail.com",
+    author="Hisham",
+    install_requires=[]
+)
